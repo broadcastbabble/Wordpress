9586aa69097b15975d730013ed8097d60b20eaf4
broadcast babble <broadcastbabble@gmail.com>
2012-10-21 11:21:13 -0500

    Added google analytics footer

--- footer.php
+++ footer.php
@@ -49,7 +49,19 @@
 <script type="text/javascript" src="http://franklin.azurewebsites.net/api/tweets?callback=twitterCallback"></script>
 <script type="text/javascript" src="http://www.google.com/jsapi?key=AIzaSyDwQwPYs2aUIeCggSaeuLxeBRRBTGUzDfw"></script>
 <script type="text/javascript" src="<?php bloginfo('stylesheet_directory'); ?>/content/calendar.js"></script>
+<script type="text/javascript">
 
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-35639877-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 <?php wp_footer(); ?>
 </body>
 </html>
